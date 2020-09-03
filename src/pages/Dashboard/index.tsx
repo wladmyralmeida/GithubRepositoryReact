a2aecc7f8d36b0e4584a88bd67bd7c6d035f1411
wladm <wladmyr@visiondevelopment.co>
2020-09-02 21:48:30 -0300

    Styled Component and Global Styled

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Title } from './styles';
+//mesma coisa que : function Dashboard (){}
+const Dashboard: React.FC = () => {
+    return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
