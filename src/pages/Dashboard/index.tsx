6251d3a7823a105b339c73239f989dd8fcc2801f
wladm <wladmyr@visiondevelopment.co>
2020-09-04 00:47:43 -0300

    Consuming API of Github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,67 @@
-import React from "react";
-import { FiChevronRight } from 'react-icons/fi';
+import React, { useState, FormEvent } from "react";
+import { FiChevronRight } from "react-icons/fi";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/Logo.svg";
 
 import { Title, Form, Repositories } from "./styles";
-//mesma coisa que : function Dashboard (){}
+
+//Não precisa colocar a tipagem de tudo que o repositório vai ter KKK;
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState("");
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    //1- Adicionar um novo repositório -> newRepo
+    //2- Consumir api do github, 3- salvar o novo repositório no estado;
+    async function handleAddRepository(event: FormEvent) {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>;
-            <Form action="">
-                <input type="text" placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    type="text"
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
             <Repositories>
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/40966093?s=460&u=fc5e9222dbfdd227ac88cad97c67819c626cb719&v=4"
-                        alt="Wlad"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Unform is a performance focused library that helps you creating beautiful forms in ReactJS, React Native with the power of uncontrolled components performance and React Hooks.</p>
-                    </div>
+                {repositories.map((repository) => (
+                    <a key={repository.full_name} href="teste">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
