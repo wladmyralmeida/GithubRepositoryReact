3eaa37d30b84f335ae0eee875c19e49c2dd7b23f
wladm <wladmyr@visiondevelopment.co>
2020-09-04 18:42:48 -0300

    Estilizando Detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import logoImg from "../../assets/Logo.svg";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +13,58 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/14101776?s=200&v=4"
+                        alt=""
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues Abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link
+                    to={`/lets/`}
+                >
+                    <div>
+                        <strong>asdfdsaf</strong>
+                        <p>asdfadsf</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
